5ca75e27d8a436a797a1e36d2705d2f82d022daf
Bemnet Dejene <bdejene19@gmail.com>
2022-02-26 15:09:00 -0500

    server is fully setup, other than the linking of handle bar files. Currently using HTML to create files => will be converted to handle bar layout/partials after (once bug solved)

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 // import necessary server components 
 const express = require('express');
-const exphbs = require('express-handlebars');
 const path = require('path')
 const sequelize = require('./config/connection');
+const exhbs = require('express-handlebars');
+const app = express();
+const hbs = exhbs.create({});
+
 
 // initialize server, handlebars and port number
 const PORT = process.env.PORT || 8000;
-const hbs = exphbs.create({});
-const app = express();
-
-// initialize handlebars as our view engine through express middleware 
+// // initialize handlebars as our view engine through express middleware 
 app.engine('handlebars', hbs.engine)
-app.set('view engine')
+app.set('view engine', 'handlebars')
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.get('/', (req, res) => {
+    res.render('checkout')
+})
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => {
         console.log(`server running on http://localhost:${PORT}`)
