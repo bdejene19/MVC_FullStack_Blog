6ed17df561ad09457a3a1ce1f1df7fdc1626a6c6
Bemnet Dejene <bdejene19@gmail.com>
2022-02-27 17:08:13 -0500

    installed bcrypt and express session. Also downgrading version of express-handlebars to use same version type as activities

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 // import necessary server components 
-const express = require('express');
-const path = require('path')
+const path = require('path');
 const sequelize = require('./config/connection');
-const exhbs = require('express-handlebars');
-const app = express();
-const hbs = exhbs.create({});
+const session = require('express-session');
 
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+const route = require('./controllers/index');
+const express = require('express');
+const app = express();
 
 // initialize server, handlebars and port number
 const PORT = process.env.PORT || 8000;
+
 // // initialize handlebars as our view engine through express middleware 
-app.engine('handlebars', hbs.engine)
-app.set('view engine', 'handlebars')
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-    res.render('checkout')
-})
+app.use(route)
+
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => {
         console.log(`server running on http://localhost:${PORT}`)
