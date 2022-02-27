6a2645ee66d373810707caab4f24a4f18a4cc433
Bemnet Dejene <bdejene19@gmail.com>
2022-02-27 17:37:46 -0500

    modularized code and began working on homepage routes

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -1,11 +1,11 @@
 const home = require('express').Router();
 
 home.get('/', (req, res) => {
-    res.render('homepage');
+    res.render('homepage', [{title: 'hello world', text: 'first post'}]);
 })
 
-home.post('/signup', (req, res) => {
-    
+home.get('/login', (req, res) => {
+    res.render('test')
 })
 
 module.exports = home;
