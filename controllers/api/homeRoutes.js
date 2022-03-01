0da22d40d2b1a7657bf7d9fe9acdb859c22f505d
Bemnet Dejene <bdejene19@gmail.com>
2022-03-01 15:51:05 -0500

    created bbusiness logic signUp route to create new User object. Has new user validation on both the frontend and backend.

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -1,4 +1,5 @@
 const home = require("express").Router();
+const User = require("../../models/User");
 
 home.get("/", (req, res) => {
   // let posts = [{title: 'hello', description: 'ayoo technology'}, {title: 'User The Champion', description: 'lord commander has returned'}, {title: 'The others', description: 'moon cake'}];
@@ -10,4 +11,16 @@ home.get("/login", (req, res) => {
   res.render("login");
 });
 
+home.post("/signUp", async (req, res) => {
+  console.log(req.body);
+  let newUser = {
+    username: req.body.username,
+    email: req.body.email,
+    full_name: req.body.name,
+    password: req.body.pswd,
+  };
+  await User.create(newUser).catch((err) => console.log(err));
+
+  console.log("success adding user from signup");
+});
 module.exports = home;
