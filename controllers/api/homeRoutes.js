255f2124a612f876d9a8de0b6e7e37d2acff2e74
Bemnet Dejene <bdejene19@gmail.com>
2022-02-28 22:50:10 -0500

    deleted test handlebar views. Also reworked some routes to handle proper view handling

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -1,11 +1,13 @@
-const home = require('express').Router();
+const home = require("express").Router();
 
-home.get('/', (req, res) => {
-    res.render('homepage', [{title: 'hello world', text: 'first post'}]);
-})
+home.get("/", (req, res) => {
+  // let posts = [{title: 'hello', description: 'ayoo technology'}, {title: 'User The Champion', description: 'lord commander has returned'}, {title: 'The others', description: 'moon cake'}];
+  // posts = posts.map(item => item.get({plain: true}))
+  res.render("homepage", { title: "chekc light", description: "hello" });
+});
 
-home.get('/login', (req, res) => {
-    res.render('test')
-})
+home.get("/login", (req, res) => {
+  res.render("login");
+});
 
 module.exports = home;
