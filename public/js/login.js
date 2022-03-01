0f6fb97630da2ac3c21c9471d0d5b0d35cc1bb2f
Bemnet Dejene <bdejene19@gmail.com>
2022-02-28 22:48:12 -0500

    worked on style sheet different views. Also began working on frontend javascript interactivity. Have finished signup function, began working on signin function. However, still need to handle backend routes to interact with database

---
+++ public/js/login.js
@@ -0,0 +1,33 @@
+const signin = document.getElementById("sign-in");
+const signup = document.getElementById("sign-up");
+
+const signUp = async (event) => {
+  event.preventDefault();
+  const name = document.getElementById("name");
+  const username = document.getElementById("new-user");
+  const pswd = document.getElementById("new-pswd");
+  console.log("fired");
+
+  let res = await fetch("/signUp", {
+    method: "POST",
+    body: JSON.stringify({ name, username, pswd }),
+    headers: { "Content-Type": "application/json" },
+  });
+  if (res.ok) {
+    console.log("hellooo");
+  } else {
+    alert("Failed to log in");
+  }
+};
+
+signup.addEventListener("click", signUp);
+
+const signIn = async (event) => {
+  event.preventDefault();
+  const username = document.getElementById("username");
+  const pswd = document.getElementById("password");
+
+  let res = await fetch("/signIn");
+};
+
+signin.addEventListener("click", signIn);
