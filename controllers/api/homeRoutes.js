62a040b443ddc74ea0101358a0a2467f2e911201
Bemnet Dejene <bdejene19@gmail.com>
2022-03-02 15:57:46 -0500

    created separate styling sheets for individual pages. Also created and finished (with small bugs) for login/signup page interactivity. Has frontend and backend (where bug is) validation

--- controllers/api/homeRoutes.js
+++ controllers/api/homeRoutes.js
@@ -1,10 +1,16 @@
 const home = require("express").Router();
-const User = require("../../models/User");
+const { User, BlogPost } = require("../../models/index");
 
-home.get("/", (req, res) => {
+home.get("/", async (req, res) => {
   // let posts = [{title: 'hello', description: 'ayoo technology'}, {title: 'User The Champion', description: 'lord commander has returned'}, {title: 'The others', description: 'moon cake'}];
   // posts = posts.map(item => item.get({plain: true}))
-  res.render("homepage", { title: "chekc light", description: "hello" });
+
+  let allBlogPosts = await BlogPost.findAll({
+    limit: 20,
+  }).catch((err) => console.log(err));
+
+  let posts = allBlogPosts.map((post) => post.get({ plain: true }));
+  res.render("homepage", { posts });
 });
 
 home.get("/login", (req, res) => {
@@ -20,7 +26,26 @@ home.post("/signUp", async (req, res) => {
     password: req.body.pswd,
   };
   await User.create(newUser).catch((err) => console.log(err));
-
   console.log("success adding user from signup");
+});
+
+home.post("/signIn", async (req, res) => {
+  const selectedUser = await User.findOne({
+    include: [{ model: BlogPost }],
+    where: {
+      email: req.body.email,
+    },
+  }).catch((err) => console.log(err));
+
+  let correctPswd = selectedUser.validPassword(req.body.pswd);
+
+  if (correctPswd) {
+    req.session.save(() => {
+      req.session.logged_in = true;
+    });
+    console.log("hit");
+    console.log("_____________________");
+  }
+  res.render("profile");
 });
 module.exports = home;
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
-const app = require('express').Router();
+const app = require("express").Router();
 
-const home = require('./api/homeRoutes')
-app.use('/', home)
+const home = require("./api/homeRoutes");
+const profile = require("./api/profileRoutes");
+app.use("/", home);
+app.use("/profile", profile);
 
 module.exports = app;
