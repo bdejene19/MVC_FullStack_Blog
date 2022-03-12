05d453bf89ba5c0f7391a49faa96a78d82da8c0b
Bemnet Dejene <bdejene19@gmail.com>
2022-03-11 19:55:04 -0500

    Finished basic routing and display of viewing specific blog posts on click. Broke down view of full article into 2 partials: 1) full-post-view, and 2) post-comments

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -1,7 +1,7 @@
 const postRouter = require("express").Router();
 const { getBlogPostById } = require("../../helpers/dbQueries");
 postRouter.get("/:id", async (req, res) => {
-  let { title, content } = await getBlogPostById(req.params.id);
-  res.render("tech-article", { title, content });
+  let { title, content, createdAt } = await getBlogPostById(req.params.id);
+  res.render("tech-article", { title, content, createdAt });
 });
 module.exports = postRouter;
