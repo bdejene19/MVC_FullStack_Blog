3f1481542ddb7bcd07c7d9d7db66705619a6dc7a
Bemnet Dejene <bdejene19@gmail.com>
2022-03-12 16:36:16 -0500

    created routes that connect frontend connectivity to make new comment => now updates database and UI.

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -6,20 +6,39 @@ const {
 } = require("../../helpers/dbQueries");
 postRouter.get("/:id", async (req, res) => {
   const techPost = await getBlogPostById(req.params.id);
-  let { title, content, createdAt, comments } = techPost;
+  const { id, title, content, createdAt, comments } = techPost;
+  const loggedIn = req.session.loggedIn;
 
-  res.render("tech-article", { title, content, createdAt, comments });
+  res.render("tech-article", {
+    id,
+    title,
+    content,
+    createdAt,
+    comments,
+    loggedIn,
+  });
 });
 
 postRouter.post("/newComment", async (req, res) => {
   if (req.session.loggedIn) {
-    let user = await findUserByEmail(req.session.email);
+    console.log("z", req.body);
+    let user = await findUserByEmail(req.session.email).catch((err) =>
+      console.log(err)
+    );
 
     if (user) {
       const postID = req.body.postID;
       const { username } = user;
       const commentText = req.body.comment;
-      let commentCreated = await makeComment(postID, username, commentText);
+      let commentCreated = await makeComment(
+        postID,
+        username,
+        commentText
+      ).catch((err) => console.log(err));
+
+      if (commentCreated) {
+        res.status(201).json({ commentCreated });
+      }
     }
   } else {
     res.status(404).json({});
