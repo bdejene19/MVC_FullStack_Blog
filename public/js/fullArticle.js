36721865e6d7b50060081594fe5a9b468f812a15
Bemnet Dejene <bdejene19@gmail.com>
2022-03-18 15:24:40 -0400

    worked on frontend interactivity

--- public/js/fullArticle.js
+++ public/js/fullArticle.js
@@ -5,20 +5,24 @@ const submitComment = async (e) => {
   e.preventDefault();
   let comment = textBoxEl.value.trim();
   if (comment !== "") {
+    console.log("not empty");
     let btnSubmit = await fetch("/posts/newComment", {
       method: "POST",
       body: JSON.stringify({ postID, comment }),
       headers: {
         "Content-type": "Application/json",
       },
-    });
+    }).catch((err) => console.log(err));
 
     if (btnSubmit.ok) {
       let newComment = await btnSubmit.json();
-
       if (newComment) {
-        window.location.replace(`/posts/${postID}`);
+        return window.location.replace(`/posts/${postID}`);
       }
+    } else {
+      let test = await btnSubmit.json();
+      console.log(test);
+      return window.location.replace(`/posts/${postID}`);
     }
   } else {
     textBoxEl.style.border = "solid red 1.5px";
